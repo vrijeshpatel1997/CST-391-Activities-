46d262f1907d3f4c08de64df0602bb35e790f469
Vrijesh Patel <60836824+vrijeshpatel1997@users.noreply.github.com>
2023-07-25 15:54:34 -0700

    Delete Activity 6 directory

--- Activity 6/NewAlbum.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const NewAlbum = () => {
-    return <div> This is a new album form</div>
-};
-
-export default NewAlbum;
--- Activity 6/dataSource.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'http://localhost:3000' // Update this baseURL if your server is running on a different domain or port
-});
-
-export default instance;
